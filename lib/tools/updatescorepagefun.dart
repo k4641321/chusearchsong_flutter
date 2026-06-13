e189cfab32c60d80c27d406e3beae01662034deb
k4641321 <k4641321@outlook.com>
2026-06-13 13:28:18 +0800

    坏掉了，上传一下重新git(

--- lib/tools/updatescorepagefun.dart
+++ lib/tools/updatescorepagefun.dart
@@ -1,27 +1,17 @@
 import 'dart:developer';
 
-import 'package:flutter_v2ray_client/flutter_v2ray.dart';
+import 'package:flutter/services.dart';
 
-Future<void> connect(String shareLink, v2ray) async {
-  try {
-    final parser = V2ray.parseFromURL(shareLink);
-    // final config = parser.getFullConfiguration();
+class V2rayService {
+  static const platform = MethodChannel(
+    'com.k4641321.chusearchsong_flutter.maimaiproxy/controller',
+  );
 
-    // await flutterVless.initializeVless(
-    //   providerBundleIdentifier: 'com.k4641321.chusearchsong_flutter',
-    //   groupIdentifier: 'group.com.k4641321.chusearchsong_flutter',
-    // );
+  static Future<void> start(String configJson) async {
+    await platform.invokeMethod('start', {'config': configJson});
+  }
 
-    await v2ray.startV2Ray(
-      remark: parser.remark,
-      // The use of parser.getFullConfiguration() is not mandatory,
-      // and you can enter the desired V2Ray configuration in JSON format
-      config: parser.getFullConfiguration(),
-      blockedApps: null,
-      bypassSubnets: null,
-      proxyOnly: false,
-    );
-  } catch (e) {
-    log('$e', name: 'updatescorepagefun.dart', level: 1000);
+  static Future<void> stop() async {
+    await platform.invokeMethod('stop');
   }
 }
